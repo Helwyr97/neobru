5bdc2247335972a4f5767b9c50ce3323b44b9c96
Helwyr97 <xoel.otero.telmo@udc.es>
2025-08-04 16:25:08 +0200

    core: add colorScheme types

--- src/components/generalTypes.ts
+++
@@ -1,2 +0,0 @@
-export type VariantType = "default" | "secondary" | "outline" | "danger";
-export type SizeType = "sm" | "md" | "lg" | "icon";
--- src/utils/common-types.ts
+++ src/utils/common-types.ts
@@ -4,10 +4,20 @@ export type CommonSizesType = (typeof CommonSizes)[number];
 export const ButtonSizes = [...CommonSizes, "icon"] as const;
 export type ButtonSizesType = (typeof ButtonSizes)[number];
 
-export const CommonVariants = [
+export const ColorSchemes = [
   "primary",
   "secondary",
   "outline",
+  "success",
   "danger",
+  "warning",
+  "info",
 ] as const;
-export type CommonVariantsType = (typeof CommonVariants)[number];
+
+export type ColorSchemesType = (typeof ColorSchemes)[number];
+
+export const Variants = [
+  "rectangular",
+  "rounded"
+] as const;
+export type VariantsType = (typeof Variants)[number];
