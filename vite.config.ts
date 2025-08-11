45ece89b5b9cf85eac7b3f8e2f6996e6816c3e73
Helwyr97 <xoel.otero.telmo@udc.es>
2025-08-11 22:48:58 +0200

    core: fix export css

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
-import { peerDependencies } from "./package.json";
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-      exclude: ["**/*.stories.ts", "**/*.test.tsx"],
-    }),
-  ],
   build: {
     lib: {
       entry: "./src/index.ts",
@@ -18,11 +11,26 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
       formats: ["cjs", "es"],
     },
+    rollupOptions: {
+      external: ["react", "react-dom", "tailwindcss"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          tailwindcss: "tailwindcss",
+        }
+      }
+    },
     sourcemap: true,
     emptyOutDir: true,
-    rollupOptions: {
-      external: Object.keys(peerDependencies),
-      output: { globals: { react: "React", "react-dom": "ReactDOM" } },
-    },
   },
+  plugins: [
+    react(),
+    dts({rollupTypes: true}),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss]
+    }
+  }
 });
